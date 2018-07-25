7228e623bb3cc1203cdd3a2c797f9c660e04fdf8
Student <student@MacBook-Pro.local>
2018-07-25 11:40:02 -0400

    update menusys

--- Menusys.py
+++ Menusys.py
@@ -1,2 +1,21 @@
 def menu ():
-    print ("Welcome ")
+    print ("Welcome,,\n 1. print Hello \n 2. print world \n 3. print python \n  4. print Hello world,welcome to my program")
+    choices = input()
+
+    if choices == "1":
+        print("PLAY")
+        menu()
+
+    if choices == "2"
+        print ("Exit")
+        menu()
+
+    if choices == "3"
+        print ()
+        menu()
+
+    if choices == "4"
+        print ("")
+        menu()
+
+menu()
