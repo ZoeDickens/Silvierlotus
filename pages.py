a70a885556c0b0b673ca998e160a19d6d790db24
Student <student@MacBook-Pro.local>
2018-07-30 14:48:41 -0400

    update

--- pages.py
+++ pages.py
@@ -290,6 +290,7 @@ class HOP4(tk.Frame):
         label.pack(side="top", fill="x", pady=10)
         button = tk.Button(self, text="continue",command=lambda:controller.show_frame("bar"))
         button.pack()
+        button.place(x=650, y=550)
 
 class exit(tk.Frame):
 
@@ -312,6 +313,7 @@ class bar(tk.Frame):
                             command=lambda: controller.show_frame("fight"))
 
         button1.pack()
+        button1.place(x=650, y=550)
 
 class fight(tk.Frame):
 
